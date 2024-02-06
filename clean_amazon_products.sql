6bcbdbaf6afd2e35e548d5bb173c53e4160d082f
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2024-02-06 16:57:38 +0700

    Remove field main_category from products

--- clean_amazon_products.sql
+++ clean_amazon_products.sql
@@ -43,7 +43,6 @@ create table if not exists ebisaan.currencies (
 create table if not exists ebisaan.products(
 	id bigserial primary key,
 	name text,
-	main_category_id integer,
 	sub_category_id integer,
 	stock_number bigint,
 	image text,
@@ -68,9 +67,9 @@ SELECT distinct main_category from raw.products;
 insert into ebisaan.sub_categories (name, main_category_id)
 select c.sub_category, mc.id from (select distinct main_category, sub_category from raw.products) c join ebisaan.main_categories mc on c.main_category = mc.name;
 
-insert into ebisaan.products (name, main_category_id, sub_category_id, stock_number, image, discount_price, actual_price, currency_id)
-select rp.name, mc.id, sc.id, ebisaan.random(1, 1000), rp.image, regexp_replace(rp.discount_price, '[₹,]', '', 'g')::numeric, regexp_replace(rp.actual_price, '[₹,]', '', 'g')::numeric, c.id
-from raw.products rp join ebisaan.main_categories mc on rp.main_category = mc.name join ebisaan.sub_categories sc on rp.sub_category = sc.name left join ebisaan.currencies c on substring(rp.actual_price from 1 for 1) = c.symbol
+insert into ebisaan.products (name, sub_category_id, stock_number, image, discount_price, actual_price, currency_id)
+select rp.name, sc.id, ebisaan.random(1, 1000), rp.image, regexp_replace(rp.discount_price, '[₹,]', '', 'g')::numeric, regexp_replace(rp.actual_price, '[₹,]', '', 'g')::numeric, c.id
+from raw.products rp left join ebisaan.sub_categories sc on rp.sub_category = sc.name left join ebisaan.currencies c on substring(rp.actual_price from 1 for 1) = c.symbol
 where rp.actual_price is not NULL;
 
 -- alter table if exists ebisaan.main_categories add constraint main_categories_name_unique_idx unique (id);
